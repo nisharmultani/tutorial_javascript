192abb75be34f8a6a77fdf8decc88b4bdf01ca23
NisharMultani <nisharmultani7@gmail.com>
2022-07-10 21:45:23 +0530

    WIP: work in laxical env

--- javascript.js
+++ javascript.js
@@ -1,14 +1,21 @@
 // closer is a function that  bind to gether with here lecsical enverment
 // function along with is lexcicas
-function a(){
-    var x = 10;
-    function b() {
-        console.log(x)
-        function c(){
-            console.log(x);
-        }
-        c();
-    }
-    b();
+
+function a() {
+  var x = 10;
+  function b() {
+    console.log(`this is  x Value ${x}`);
+  }
+  b();
 }
 a();
+
+
+
+
+// const x = 100;
+// function a(){
+//     return x;
+// }
+//  const v = a();
+//  console.log(v)
