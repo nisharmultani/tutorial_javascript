c3537a3f2577ebfc18eb51e60303c6b6073c13fd
NisharMultani <nisharmultani7@gmail.com>
2022-07-12 19:38:08 +0530

    try:array unshift function

--- javascript.js
+++ javascript.js
@@ -1,45 +1,22 @@
-// closer is a function that  bind to gether with here lecsical enverment
-// function along with is lexcicas
-
-function a() {
-  var x = 10;
-  function b() {
-    console.log(`this is  x Value ${x}`);
-  }
-  b();
-}
-a();
-//  prototype
-
-
-//  let arr = ['nsihar','multani']
-// info = {
-//   name: 'nishar',
-//   lastName: 'multani'
-// }
-// function a(){
-//   //
-
-// }
-
-
-
-
-// const x = 100;
-// function a(){
-//     return x;
-// }
-//  const v = a();
-//  console.log(v)
 
 const arr = ['nishar' , 'multani','gujarat','india']
 // array built in function  
+
 // 1) array value 
 // console.log(arr.values())
 // const values = arr.values()
 for (let index of arr.values() ){
   console.log(index)
 }
+//  2)unshift
+// The arr.unshift() method is used to add one or more elements to the beginning of the given array
+// we add new Strign in array in bigining
+arr.unshift('firstadd','second add')
+console.log(arr)
+for ( let index of arr.values()){
+  console.log(index)
+}
+
 
 
 
