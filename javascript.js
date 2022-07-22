cbdef6fe78e0183b8f2377afd21e473c7bb62659
NisharMultani <nisharmultani7@gmail.com>
2022-07-22 10:57:39 +0530

    try: i dont know what i did

--- javascript.js
+++ javascript.js
@@ -1,5 +1,49 @@
 
-const arr = ['nishar' , 'multani','gujarat','india']
+
+// const arr = ['nishar' , 'multani','gujarat','india']
+// const arr = [2,3,4,5,6,7,]
+// const arr2 =[{name:'nisa',addres:'suhari', agr:50},
+// {name:'nishra',addres:'buhari', agr:40},
+// {name:'sra',addres:'hari', agr:40},
+// {name:'hra',addres:'buri', agr:30},
+// {name:'nira',addres:'bri', agr:60},]
+
+// map function
+
+
+// const result  = arr.map((x) => x >2 )
+//  console.log(result)
+
+// const filter  = arr.filter((x) => x%2)
+// console.log(filter)
+
+// function sum (arr){
+//   let s = 0 ;
+//   const arrlength =  arr.length
+//  for (let i = 0; i < arrlength; i++) {
+//     s = s + arr[i];
+//   }
+//   return s;
+// }
+
+// console.log(sum(arr))
+
+// const x  = arr.reduce(function(start , current ){
+//  start = start +current
+//  return start
+// },0)
+// console.log(x)
+
+// const result = arr2.map((x)=>{
+//    return x.addres
+// })
+// console.log(result)
+
+// const con = arr2.filter((x)=>{
+//   return x.agr>30 x.name
+//   }
+// })
+// console.log(con)
 // array built in function  
 
 // 1) array value 
@@ -37,8 +81,9 @@ const arr = ['nishar' , 'multani','gujarat','india']
 // by default is will arrange in asseding order
 // console.log(arr.sort())
 // const person = {
+//   value : 53,
 //   fullName: function(address , pincode) {
-//     return this.firstName + this.lastName +address + pincode
+//     return this.firstName + this.lastName +address + this.value + pincode
 //   }
 // }
 // const persion1 = {
@@ -46,9 +91,9 @@ const arr = ['nishar' , 'multani','gujarat','india']
 //   lastName : 'multani'
 // }
 // apply method tack argument in form of array
-// const a = person.fullName.apply(persion1,['buhari','93489759'])s
+// const a = person.fullName.apply(persion1,['buhari','93489759'])
 // console.log(a)
-// //call methods tack argument in a Strig form 
+//call methods tack argument in a Strig form 
 // const b = person.fullName.call(persion1,'valod','983450')
 // console.log(b)
 
@@ -62,34 +107,36 @@ const arr = ['nishar' , 'multani','gujarat','india']
 // console.log('call api', count++)
 
 // binary search 
-const arr2 = [2,4,6,8,12,18];
-const size = arr2.length
-const key  = 2;
+// const arr2 = [2,4,6,8,12,18];
+// const size = arr2.length
+// const key  = 2;
 
-function findvalue(arr2, size ,key){
- let start = 0;
- let end = size-1;
- let mid = Math.floor((start + end)/2) ;
+// function findvalue(arr2, size ,key){
+//  let start = 0;
+//  let end = size-1;
+//  let mid = Math.floor((start + end)/2) ;
 
- while(start <= end){
-   if(arr2[mid] == key){
-     return `this is ${key} the key is ${mid}`
-  }
-  // for rightside
-  if(key > arr2[mid]){
-    start = mid + 1;
-  }
-  else{
-    end = mid-1;
-  }
-  mid = Math.floor((start + end)/2);
-}
- return 'no value found';
+//  while(start <= end){
+//    if(arr2[mid] == key){
+//      return `this is ${key} the key is ${mid}`
+//   }
+//   // for rightside
+//   if(key > arr2[mid]){
+//     start = mid + 1;
+//   }
+//   else{
+//     end = mid-1;
+//   }
+//   mid = Math.floor((start + end)/2);
+// }
+//  return 'no value found';
   
-}
+// }
 
- const result = findvalue(arr2,size,key)
- console.log(result)
+//  const result = findvalue(arr2,size,key)
+//  console.log(result)
+
+// first and last occuurance of array
 
 
 
